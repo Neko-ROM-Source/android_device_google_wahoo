b49199aa06fb3d409cd920a5c91da1ae232e3701
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    wahoo: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- device.mk
+++ device.mk
@@ -260,6 +260,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio Hal
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay
+
+PRODUCT_PACKAGES += \
     android.hardware.radio@1.1.vendor \
     android.hardware.radio.config@1.0 \
     librmnetctl \
