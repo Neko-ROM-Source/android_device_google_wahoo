14631dc8ec1ed0ec68bfb41ad152bfc6f9249fba
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-12 04:06:13 -0500

    wahoo: Kill deprecated TARGET_USES_MKE2FS
    * This has been useless since some time in Pie.
    
    Change-Id: Ic02238aa0f1c25cfad20a2b827c3c2fe128f3543

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,9 +179,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := device/google/wahoo/configs/manifests/framewor
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/wahoo/configs/manifests/device_framework_matrix.xml
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
-# Use mke2fs to create ext4 images
-TARGET_USES_MKE2FS := true
-
 BOARD_VNDK_VERSION := current
 
 # Board uses A/B OTA.
